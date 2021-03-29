4a671f7f3836954d6e1b429d248c1a1d4c7187ea
Mohamed A. Negm <mr.negm90@gmail.com>
2021-03-28 21:20:48 -0700

    stock picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,14 +1,12 @@
 def stock_picker(array)
     
-    sorted_arr = []
-    answer = []
-    sorted_arr = array.sort 
-    i = 0
+    sorted_arr = array.sort  #sorting the array in a temp sorted array
+    i = 0 
     
     while i < array.length
-        if array.index(sorted_arr[-1]) > array.index(sorted_arr[0])
-            return answer << sorted_arr[0] << sorted_arr[-1]
-        else
+        if array.index(sorted_arr[-1]) > array.index(sorted_arr[0]) #if the index of the largest number is greater then the index of the smaller number, it means that the smaller came first which means you can buy it first
+            return [sorted_arr[0], sorted_arr[-1]]
+        else #if the largest number came before the smallest number then remove the largest and smallest and try the next pair
             sorted_arr.pop
             sorted_arr.shift
         end
@@ -17,4 +15,4 @@ def stock_picker(array)
 end
 
 
-print stock_picker([17,3,6,9,15,8,6,1,10])
+print stock_picker([17,3,6,9,15,8,6,1,10]) 
